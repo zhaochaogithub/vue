7ea4e4b2e67855d086bd95ea65660504f6806b8e
zhaochao <874107619@qq.com>
2021-01-10 21:05:32 +0800

    项目打包以及发布

---
+++ vue_demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img class="logo" src="./assets/logo.png" alt="" srcset="">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld'
+  export default {
+    data () {
+      return {
+        
+      }
+    },
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+
+<style>
+  .logo {
+    width: 100px;
+    height: 100px;
+  }
+</style>
---
+++ vue_demo/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p class="msg">{{msg}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Hello Vue!'
+      }
+    }
+  }
+</script>
+
+<style>
+  .msg {
+    color: red;
+  }
+</style>
